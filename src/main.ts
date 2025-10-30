20b2d20d40a5dd0ee380b664a6a78fb3321c1b2e
Irel Kian <116436224+irel04@users.noreply.github.com>
2025-10-30 15:31:19 +0800

    build: add swagger module

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,18 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     transform: true
   }));
+
+  // Swagger Ui
+  const config = new DocumentBuilder()
+  .setTitle("DonaT API Open API")
+  .setDescription("Official Documentation for the Project")
+  .setVersion('1.0')
+  .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
